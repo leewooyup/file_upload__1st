6eeae5e6da532e5b6de99847f958b6ed3d5a7c9a
leewooyup <zjadlspun1114@naver.com>
2022-09-05 15:06:00 +0900

    추가세팅

---
+++ src/main/java/com/ll/exam/app10/app/base/entity/BaseEntity.java
@@ -0,0 +1,33 @@
+package com.ll.exam.app10.app.base.entity;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Entity;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+
+import static javax.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor(access = PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+}
